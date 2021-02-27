b96c2f859f0adf2f15fcb9c185b96e1b073976c6
Rusben Guzman <rusbenguzman@gmail.com>
2021-02-27 13:52:27 -0500

    Added music background and created audio controller to fade in/out the music

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+
+    // Components vars
+    private AudioSource _audioSource;
+    
+
+    // General vars
+    private float _min = 0;
+    private float _max = 1;
+    [SerializeField]
+    private int _fadeSpeed = 1;
+    public bool fadeIn = true;
+
+
+    private void Awake() 
+    {
+        _audioSource = GetComponent<AudioSource>();    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for fade
+        if (fadeIn)
+        {
+            FadeIn();
+        } else 
+        {
+            FadeOut();
+        }
+
+    }
+
+    public void FadeIn()
+    {
+        if (_audioSource.volume < _max)
+        {
+            _audioSource.volume += _fadeSpeed * Time.deltaTime;
+        }
+    }
+
+    public void FadeOut()
+    {
+        if (_audioSource.volume > _min) 
+        {
+            _audioSource.volume -= _fadeSpeed * Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -10,6 +10,7 @@ public class IntroManager : MonoBehaviour
     // UI vars
     public TextMeshProUGUI dialog1, dialog2, dialog3;
     public GameObject dialog2Panel;
+    private Animator dialog2PanelAnim;
 
 
     // General vars
@@ -20,6 +21,7 @@ public class IntroManager : MonoBehaviour
     {
         // Run first dialog
         StartCoroutine(StartDialog(dialog1));
+        dialog2PanelAnim = dialog2Panel.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -34,6 +36,7 @@ public class IntroManager : MonoBehaviour
             // Start dialgo2
             dialog2Panel.SetActive(true);
             dialog2.transform.parent.gameObject.SetActive(true);
+            dialog2PanelAnim.Play("panel-in");
             StartCoroutine(StartDialog(dialog2));
         }
 
@@ -42,6 +45,7 @@ public class IntroManager : MonoBehaviour
         {
             // Disable dialog 2
             dialog2.GetComponent<DialogController>().finished = false;
+            dialog2PanelAnim.Play("panel-out");
             dialog2.gameObject.SetActive(false);
             dialog2Panel.SetActive(false);
             // Start dialog 3
