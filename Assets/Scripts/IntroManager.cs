2b7ec3bd8ce775f21fa2bab1c562f26bbc38ec5f
Rusben Guzman <rusbenguzman@gmail.com>
2021-02-27 11:45:19 -0500

    Make dialog prefab and created intro manager

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class IntroManager : MonoBehaviour
 {
 
     // UI vars
-    
+    public TextMeshProUGUI dialog1, dialog2, dialog3;
+
+
+    // General vars
+    private float _dialogDelay = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Run first dialog
+        StartCoroutine(StartDialog(dialog1));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator StartDialog(TextMeshProUGUI  dialog)
+    {
+        yield return new WaitForSeconds(_dialogDelay);
+        dialog.GetComponent<DialogController>().StartDialog();
     }
 }
