5e522114aee50f42918355def0853adb5af95f6f
Rusben Guzman <rusbenguzman@gmail.com>
2021-02-27 09:07:06 -0500

    Created text dialog display and typing effect

---
+++ Assets/Scripts/DialogController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogController : MonoBehaviour
+{
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    [SerializeField]
+    private float _typingDelay = 0.05f;
+
+
+    private void Start() {
+        StartCoroutine(Type());
+    }
+
+    // Text typing effect
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(_typingDelay);
+        }
+
+    }
+
+}
