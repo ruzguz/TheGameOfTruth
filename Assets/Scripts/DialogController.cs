c36d08869d2f88922d0cb25ce1d90f8820a0c9b5
Rusben Guzman <rusbenguzman@gmail.com>
2021-02-27 09:58:32 -0500

    Created continue dialog button

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -1,31 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DialogController : MonoBehaviour
 {
 
-    public TextMeshProUGUI textDisplay;
+
+    // UI vars
+    private TextMeshProUGUI _textDisplay;
+    public GameObject continueBtn;
+
+    // General vars
     public string[] sentences;
-    private int index;
+    private int _index;
     [SerializeField]
     private float _typingDelay = 0.05f;
 
 
     private void Start() {
-        StartCoroutine(Type());
+        _textDisplay = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(Type());        
+    }
+
+    private void Update() {
+        // Check if current text displayed is complete
+        if (_textDisplay.text == sentences[_index])
+        {
+            continueBtn.SetActive(true);
+        }
     }
 
     // Text typing effect
     IEnumerator Type()
     {
-        foreach(char letter in sentences[index].ToCharArray())
+        foreach(char letter in sentences[_index].ToCharArray())
         {
-            textDisplay.text += letter;
             yield return new WaitForSeconds(_typingDelay);
+            _textDisplay.text += letter;
+            
         }
+    }
 
+    // Show next sentence in the dialog
+    public void NextSentence()
+    {
+        // Hide coninue button
+        continueBtn.SetActive(false);
+        if (_index < sentences.Length - 1)
+        {
+            _index++;
+            _textDisplay.text = "";
+            StartCoroutine(Type());
+        } else 
+        {
+            _textDisplay.text = "";
+        }
     }
 
 }
