24c5d1f28125ea848dd23432e7edc546003843cf
Rusben Guzman <rusbenguzman@gmail.com>
2021-02-28 14:26:06 -0500

    Added collider to level elements and add player movement function

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    // General vars
+    public float playerSpeed = 5;
+    private float _h,_v;
+    private Vector3 _moveDirection;
+
+
+    // Components vars
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player movement
+        _h = Input.GetAxis("Horizontal");
+        _v = Input.GetAxis("Vertical");
+
+        _moveDirection.x = _h;
+        _moveDirection.y = _v;
+
+        transform.position += _moveDirection * Time.deltaTime * playerSpeed;
+
+    }
+
+    private void Run()
+    {
+        
+    }
+}
