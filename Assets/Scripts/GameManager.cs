be6ef0c4164eeed766d04e129f757a7798f37bdb
Rusben Guzman <rusbenguzman@gmail.com>
2021-03-02 04:18:15 -0500

    Created game manager script and make it a singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    // General vars
+    public static GameManager sharedInstance;
+    public int difficulty;
+
+    // Level vars
+    public int ghostQuantity;
+    public int ghostLevelLives;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Singleton initialization
+        if (sharedInstance == null) 
+        {
+            sharedInstance = this;
+        } else 
+        {
+            Destroy(gameObject);
+        }
+
+        // TODO: set level values according to the difficulty
     }
 
     // Update is called once per frame
