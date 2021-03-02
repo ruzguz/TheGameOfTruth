7ca5a0891c4a9c10c4a65adc7d4b93add0b01847
Rusben Guzman <rusbenguzman@gmail.com>
2021-03-02 04:28:15 -0500

    Ghost follows patient

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+
+    // General vars
+    public float speed = 1;
+    private GameObject _patient;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _patient = GameObject.Find("Patient");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move to the patient
+        transform.position = Vector3.MoveTowards(transform.position, _patient.transform.position, speed * Time.deltaTime);
+    }
+}
