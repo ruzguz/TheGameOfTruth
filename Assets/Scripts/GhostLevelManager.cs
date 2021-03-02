377aeb93cd5590f89574f5938d3df3515001fa77
Rusben Guzman <rusbenguzman@gmail.com>
2021-03-02 04:18:48 -0500

    Created ghost level and elements

---
+++ Assets/Scripts/GhostLevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostLevelManager : MonoBehaviour
+{
+
+    // Level config vars
+    [SerializeField]
+    private int _ghostQuantity = 10;
+    [SerializeField]
+    private int _patientLives = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_ghostQuantity = GameManager.sharedInstance.ghostQuantity;
+        //_patientLives = GameManager.sharedInstance.ghostLevelLives;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
