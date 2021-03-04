d7c3ebfed5e58eacf5eb088ba3bdcfd78566203e
Rusben Guzman <rusbenguzman@gmail.com>
2021-03-04 03:17:22 -0500

    Generate random blocks for each game and change mouse cursor

--- Assets/Scripts/GhostLevelManager.cs
+++ Assets/Scripts/GhostLevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GhostLevelManager : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class GhostLevelManager : MonoBehaviour
     public GameObject transitionPanelLose;
 
     // Level config vars
+    private bool _canGenerate2nd = true;
+    private bool _canGenerate3rd = true;
+    private GameObject _currentBlock;
+    public float limit1 = 0.3f;
+    public float limit2 = 0.6f;
+    public GameObject[] blocks;
+    
+    private int _blocksQty = 3;
+    public int[] blocksForLevel;
     public static GhostLevelManager sharedInstance;
     public int ghostQuantity = 10;
     public int ghostDestroyed = 0;
@@ -42,6 +52,14 @@ public class GhostLevelManager : MonoBehaviour
 
         Cursor.SetCursor(cursorTexture, _hotSpot, _cursorMode);
 
+        // Generate blocks for the game
+        blocksForLevel = new int[_blocksQty];
+        GenerateBlocks();
+
+        // Show first block
+        _currentBlock = Instantiate(blocks[blocksForLevel[0]], Vector3.zero, Quaternion.identity);
+        
+
     }
 
     // Update is called once per frame
@@ -59,12 +77,31 @@ public class GhostLevelManager : MonoBehaviour
             _timer = spawnTime;
         }
 
+        Debug.Log((int)(ghostQuantity*limit2));
+
+        // Transition form first block to second
+        if (ghostDestroyed >= (int)(ghostQuantity*limit1) && ghostDestroyed < (int)(ghostQuantity*limit2) && _canGenerate2nd) 
+        {
+            Debug.Log("Second Block");
+            _currentBlock.SetActive(false);
+            _currentBlock = Instantiate(blocks[blocksForLevel[1]], Vector3.zero, Quaternion.identity);
+            _canGenerate2nd = false;
+            
+        }
+
+        // Transition from second block to third
+        if (ghostDestroyed >= ghostQuantity*limit2 && _canGenerate3rd) 
+        {
+            Debug.Log("Third Block");
+            _currentBlock.SetActive(false);
+            _currentBlock = Instantiate(blocks[blocksForLevel[2]], Vector3.zero, Quaternion.identity);
+            _canGenerate3rd = false;
+        }
 
         // Win condition
         if(ghostDestroyed >= ghostQuantity)
         {
             // TODO: go to win scene
-            Debug.Log("Win game");
             transitionPanelWin.SetActive(true);
             transitionPanelWin.GetComponent<Animator>().Play("panel-in");
 
@@ -75,9 +112,20 @@ public class GhostLevelManager : MonoBehaviour
         if (patientLives <= 0) 
         {
             // TODO: go to lose scene
-            Debug.Log("Lose Game");
             transitionPanelLose.SetActive(true);
             transitionPanelLose.GetComponent<Animator>().Play("panel-in");
+        }
+    }
+
+    void GenerateBlocks()
+    {
+        for (int i = 0; i < _blocksQty; i++) 
+        {
+            int randomNumber = -1;
+            while(blocksForLevel.Contains(randomNumber) || randomNumber == -1) {
+                randomNumber = Random.Range(0, blocks.Length);
+            }
+            blocksForLevel[i] = randomNumber;
         }
     }
 
